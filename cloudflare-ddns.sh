9b30c1e4933168c1a11745f2ba44c9a46a361751
hoantd <hoantd@coccoc.com>
2022-12-25 02:54:11 +0700

    cloudflare ddns

---
+++ cloudflare-ddns.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# A bash script to update a Cloudflare DNS A record with the external IP of the source machine
+zone=example.com
+# dnsrecord is the A record which will be updated
+dnsrecord=example.com
+
+## Cloudflare authentication details
+## keep these private
+cloudflare_auth_email=mail@gmail.com
+cloudflare_auth_key=3a37easdfawef22ce2
+
+
+# Get the current external IP address
+ip=$(curl -s -X GET https://checkip.amazonaws.com)
+datee=$(date)
+echo "Current IP is $ip"
+
+if host $dnsrecord 1.1.1.1 | grep "has address" | grep "$ip"; then
+  echo "$dnsrecord is currently set to $ip; no changes needed"
+  exit
+fi
+
+# if here, the dns record needs updating
+
+# get the zone id for the requested zone
+zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
+  -H "X-Auth-Email: $cloudflare_auth_email" \
+  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+
+echo "Zoneid for $zone is $zoneid"
+
+# get the dns record id
+dnsrecordid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$dnsrecord" \
+  -H "X-Auth-Email: $cloudflare_auth_email" \
+  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+
+echo "DNSrecordid for $dnsrecord is $dnsrecordid"
+
+# update the record
+curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid" \
+  -H "X-Auth-Email: $cloudflare_auth_email" \
+  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Content-Type: application/json" \
+  --data "{\"type\":\"A\",\"name\":\"$dnsrecord\",\"content\":\"$ip\",\"ttl\":1,\"proxied\":false,\"comment\":\"$datee\"}" | jq
